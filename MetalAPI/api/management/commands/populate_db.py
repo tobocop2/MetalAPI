7a0b4c2c028df896e42d00cde672ae81b979a08d
Tobias <tobias@perelste.in>
2015-01-10 07:42:42 +0000

    Modify populaton script to allow for resuming

--- MetalAPI/api/management/commands/populate_db.py
+++ MetalAPI/api/management/commands/populate_db.py
@@ -2,53 +2,89 @@ from django.core.management.base import BaseCommand
 from api.models import Band, Release, BandLineup, ReleaseLineup, BandMusician,\
         ReleaseMusician, Song, SimilarArtist, RelatedLinks
 from django.core import management
+from django.db import transaction
 import json
 import os
 import glob
 
+path = "/home/tobias/chunks/todo/*.json"
 #path = "/home/tobias/chunks/*.json"
-path = "/home/tobias/z_test/z_test.json"
+#path = "/home/tobias/chunks/ma.json"
 
 class Command(BaseCommand):
     #args = '<foo bar ...>'
     #help = 'our help string comes here'
+    #parsed_files = []
 
+    #@transaction.atomic
     def add_bands(self):
-        for fname in glob.glob(path):
-            data = open(fname)
-            json_data = json.load(data)
+        #bulk_bands = []
+        for fname in sorted(glob.glob(path)):
+            #if fname not in self.parsed_files:
+            with open(fname) as data: json_data = json.load(data)
             for band in json_data:
-                ma_id = band['id']
-                name = band['name']
-                url_name = name.replace(' ', '_')
-                url = 'http://www.metal-archives.com/bands/%s/%s' % (url_name, ma_id)
-                country = band['country']
-                status = band['status']
-                lyrical_themes = band['lyrical_themes']
-                formation_year = band['formation_year']
-                current_label = band['current_label']
-                years_active = band['years_active']
-                location = band['location']
-                genre = band['genre']
-                description = band['description']
-                b=Band(ma_id=ma_id, name=name, url=url, country=country, status=status,\
-                        lyrical_themes=lyrical_themes, formation_year=formation_year,\
-                        current_label=current_label, years_active=years_active,\
-                        location=location, genre=genre, description=description)
-                b.save()
+                try:
+                    ma_id = band['id']
+                    print 'Trying to add id: %s' % ma_id
+                    print 'In file: '+fname
+                    Band.objects.get(ma_id=ma_id)
+                except:
+                    name = band['name']
+                    print 'Adding band: %s' % name
+                    url_name = name.replace(' ', '_')
+                    url = 'http://www.metal-archives.com/bands/%s/%s' % (url_name, ma_id)
+                    country = band['country']
+                    status = band['status']
+                    lyrical_themes = band['lyrical_themes']
+                    formation_year = band['formation_year']
+                    current_label = band['current_label']
+                    years_active = band['years_active']
+                    location = band['location']
+                    genre = band['genre']
+                    description = band['description']
+                    b=Band(
+                            ma_id=ma_id,
+                            name=name,
+                            url=url,
+                            country=country,
+                            status=status,
+                            lyrical_themes=lyrical_themes,
+                            formation_year=formation_year,
+                            current_label=current_label,
+                            years_active=years_active,
+                            location=location, genre=genre,
+                            description=description
+                    )
+                    #bulk_bands.append(b)
+                    b.save()
 
-                # Create foreign key relationships for this band's albums
-                self.add_releases(band)
-                self.add_band_lineups(band)
-                self.add_similar_artists(band)
-                self.add_related_links(band)
+                    # Create foreign key relationships for this band's albums
+                    self.add_releases(band,fname)
+                    self.add_band_lineups(band)
+                    self.add_similar_artists(band)
+                    self.add_related_links(band)
 
-    def add_releases(self, band):
+        #Band.objects.bulk_create(bulk_bands)
+
+                #except Exception as e:
+                #    if 'invalid literal' in str(e.message):
+                #        with open('exceptions.log','a') as f:
+                #            f.write('Error in file: %s, Band Url: %s\n Error Message: %s' % (fname, band.url, e.message))
+                #    print e.message
+                #    continue
+
+            #self.parsed_files.append(fname)
+            #print 'Files Parsed: %d' % len(self.parsed_files)
+       # print 'Total files Parsed: %d' % len(self.parsed_files)
+
+    def add_releases(self, band,fname):
+        #bulk_releases = []
         for release in band['detailed_discography']:
             # Removing 'parsed' key to account for Tobias' horribly architected data.
             release.pop('parsed', None)
 
             name = release.keys()[0]
+            print 'Adding release name: %s' % name
             r = Release(
                 band=Band.objects.get(ma_id=band['id']),
                 name=name,
@@ -58,12 +94,20 @@ class Command(BaseCommand):
                 release_type=release[name]['type'],
                 release_year=release[name]['year']
             )
-
+            print 'The release '+r.band.url
+            print 'The file: '+fname
+            #try:
+            #Release.get(release_id=r.release_id)
             r.save()
+            #bulk_releases.append(r)
             self.add_songs(release, r.release_id)
             self.add_release_lineup(release, r.release_id)
+            #except:
+            #    continue
+        #Release.objects.bulk_create(bulk_releases)
 
     def add_songs(self, release, release_id):
+        #bulk_songs = []
         for song in release[release.keys()[0]]['songs']:
             s = Song(
                 release=Release.objects.filter(release_id=release_id).first(),
@@ -72,11 +116,13 @@ class Command(BaseCommand):
                 lyrics=song[song.keys()[0]]['lyrics'],
                 name=song.keys()[0]
             )
-
             s.save()
+            #bulk_songs.append(s)
+        #Song.objects.bulk_create(bulk_songs)
 
     def add_release_lineup(self, release, release_id):
         # Create the foreign key release lineup object
+        #bulk_release_musicians = []
         l = ReleaseLineup(
             release=Release.objects.filter(release_id=release_id).first(),
         )
@@ -88,8 +134,9 @@ class Command(BaseCommand):
                 name=musician.keys()[0],
                 role=musician[musician.keys()[0]]
             )
-
             m.save()
+            #bulk_release_musicians.append(m)
+        #ReleaseMusician.objects.bulk_create(bulk_relese_musicians)
 
     def add_band_lineups(self, band):
         self.add_band_lineup(band, band['lineup']['complete_lineup'], BandLineup.COMPLETE)
@@ -98,6 +145,7 @@ class Command(BaseCommand):
         self.add_band_lineup(band, band['lineup']['past_lineup'], BandLineup.PAST)
 
     def add_band_lineup(self, band, lineup, lineup_type):
+        #bulk_band_musicians = []
         l = BandLineup(
             band=Band.objects.get(ma_id = band['id']),
             lineup_type=lineup_type
@@ -111,10 +159,13 @@ class Command(BaseCommand):
                 name=musician.keys()[0],
                 role=musician[musician.keys()[0]]
             )
-
             m.save()
+            #bulk_band_musicians.append(m)
+
+        #BandMusician.objects.bulk_create(bulk_band_musicians)
 
     def add_similar_artists(self, band):
+        #bulk_similar_artists = []
         for artist in band['similar_artists']:
             fields = {
                 'name': artist.keys()[0],
@@ -133,9 +184,12 @@ class Command(BaseCommand):
 
             s = SimilarArtist(**fields)
             s.save()
+            #bulk_similar_artists.append(s)
+        #SimilarArtist.objects.bulk_create(bulk_similar_artists)
 
     def add_related_links(self, band):
         #link_type is official,tabs etc
+        #bulk_related_links = []
         fields = {
             'band': Band.objects.get(ma_id=band['id']),
         }
@@ -149,6 +203,8 @@ class Command(BaseCommand):
                     fields['category'] = link_category
                     rl = RelatedLinks(**fields)
                     rl.save()
+                    #bulk_related_links.append(rl)
+        #RelatedLinks.objects.bulk_create(bulk_related_links)
 
     def handle(self, *args, **options):
         self.add_bands()
