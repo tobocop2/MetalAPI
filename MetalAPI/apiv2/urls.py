d6065b0109c92a1f11342954d94ed5672ecb5abf
T-G-P <tobias.perelstein@gmail.com>
2015-11-29 16:14:23 -0500

    Add new api urls

---
+++ MetalAPI/apiv2/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'bands', views.BandViewSet)
+router.register(r'lineups', views.LineupViewSet)
+router.register(r'releases', views.ReleaseViewSet)
+router.register(r'musicians', views.MusicianViewSet)
+router.register(r'songs', views.SongViewSet)
+router.register(r'relatedlinks', views.RelatedLinkViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/',
+        include('rest_framework.urls', namespace='rest_framework'))
+]
