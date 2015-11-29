12523dbd769e5a7552778d1822821d035a690f83
T-G-P <tobias.perelstein@gmail.com>
2015-11-29 16:15:28 -0500

    Update project urls

--- MetalAPI/MetalAPI/urls.py
+++ MetalAPI/MetalAPI/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    url(r'^$', include('api.urls')),
-    url(r'^api/', include('api.urls')),
+urlpatterns = patterns(
+    '',
+    # url(r'^$', include('api.urls')),
+    # url(r'^api/', include('api.urls')),
+    url(r'^apiv2/', include('apiv2.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
