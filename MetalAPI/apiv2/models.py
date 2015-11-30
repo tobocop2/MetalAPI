9a9c1f6b75ec30d43cc756bb604ba2f23670cabf
T-G-P <tobias.perelstein@gmail.com>
2015-11-30 00:26:50 -0500

    Update apiv2 models

--- MetalAPI/apiv2/models.py
+++ MetalAPI/apiv2/models.py
@@ -7,7 +7,7 @@ class Band(models.Model):
                                            related_name='similar_to')
     ma_id = models.BigIntegerField()
     name = models.TextField()
-    url = models.URLField()
+    ma_url = models.URLField()
     country = models.CharField(max_length=200)
     status = models.CharField(max_length=200)
     lyrical_themes = models.CharField(max_length=200)
@@ -28,20 +28,25 @@ class Lineup(models.Model):
     CURRENT = 'CR'
     LIVE = 'LI'
     PAST = 'PA'
+    COMPLETE = 'CT'
     UNKNOWN = 'UN'
     LINEUP_TYPES = (
         (CURRENT, 'current'),
         (LIVE, 'live'),
         (PAST, 'past'),
+        (COMPLETE, 'complete'),
         (UNKNOWN, 'unknown'),
     )
 
-    band = models.ForeignKey(Band)
-    lineup_type = models.CharField(max_length=2, choices=LINEUP_TYPES)
+    band = models.ForeignKey(Band, related_name='lineups')
+    lineup_type = models.CharField(max_length=2,
+                                   choices=LINEUP_TYPES,
+                                   default=CURRENT)
 
 
 class Release(models.Model):
-    lineup = models.ForeignKey(Lineup)
+    lineup = models.ForeignKey(Lineup, related_name='lineup')
+    # band = models.ForeignKey(Band, related_name='releases')
     ma_release_id = models.BigIntegerField()
     name = models.TextField()
     notes = models.TextField()
@@ -54,19 +59,26 @@ class Release(models.Model):
 class Musician(models.Model):
     name = models.TextField()
     role = models.TextField()
-    lineup = models.ManyToManyField(Lineup)
+    ma_musician_id = models.BigIntegerField()
+    lineup = models.ManyToManyField(Lineup, related_name='musicians')
 
 
 class Track(models.Model):
-    release = models.ForeignKey(Release)
+    release = models.ForeignKey(Release, related_name='tracks')
     track_number = models.IntegerField()
     name = models.TextField()
     length = models.CharField(max_length=200)
     lyrics = models.TextField()
 
+    class Meta:
+        ordering = ['track_number']
+
+        def __unicode__(self):
+            return '%d: %s' % (self.track_number, self.name)
+
 
 class RelatedLink(models.Model):
-    band = models.ForeignKey(Band)
+    band = models.ForeignKey(Band, related_name='related_links')
     category = models.CharField(max_length=200)
     link_type = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
